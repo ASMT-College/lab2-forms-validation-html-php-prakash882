9976cf97de42f916e5ca056d02c980122c326992
prakash882 <pt422194@gmail.com>
2023-09-16 20:39:53 +0545

    modify

--- lab2_validation_form.php
+++ lab2_validation_form.php
@@ -6,29 +6,42 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="get" onsubmit="return Validateform()">
+    <?php
+        if(isset($_GET['submit'])){
+            $email2 = $_GET['email2'];
+            $regexEmail = "/[A-Za-z]+@[A-Za-z0-9]+.[A-Za-z]{2,3}/";
+            if(preg_match($regexEmail, $email2)){
+                echo"Signup Completed";
+            }
+            else{
+                echo"error occured";
+            }
+        }
+    ?>
+    <form action="" method="get">
         <div>Signup</div>
         <div id="emailError"></div>
         <div>Email2: <input type="text" name="email2" id="email2" required></div>
         <div>Password: <input type="password" name="pw" id="password" required></div>
         <div>Email: <input type="email" name="email" id="email" required></div>
-        <div><button type="submit">Signup</button></div>
+        <div><button name="submit" type="submit">Signup</button></div>
+        <div id="emailvalid"></div>
     </form>
 </body>
-<script>
+<!-- <script>
     function Validateform(){
         var emaildata = document.getElementById("email").value
         var passworddata = document.getElementById("password").value
         var email2 = document.getElementById("email2").value
         // var regexEmail = new RegExp('[A-Za-z]+@[A-Za-z0-9]+.[A-Za-z]{2,3}')
         var regexEmail = /[A-Za-z]+@[A-Za-z0-9]+.[A-Za-z]{2,3}/;
-        if(regexEmail.text(email2)){
+        if(regexEmail.test(email2)){
             return true;
         }
         else{
-            document.getElementById("emailError").innerHTML = "Invalid email";
+            document.getElementById("emailvalid").innerHTML = "Invalid email";
             return false;
         }
     }
-</script>
+</script> -->
 </html>
